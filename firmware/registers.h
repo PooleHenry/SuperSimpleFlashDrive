ad7a1c04e0aab9aad79cf36aeeca7e4df29552d9
PooleHenry <poolehenrym@gmail.com>
2026-09-05 13:51:37 -0700

    Create registers.h

---
+++ firmware/registers.h
@@ -0,0 +1,12 @@
+// Defintions for the special function registers I am using in the project, based on the CH552 datasheet.
+// This is using syntax for the SDCC compiler.
+
+
+// Port 3
+__sfr __at (0xB0) P3;
+__sfr __at (0x9E) P3_MOD_OC;
+__sfr __at (0x9F) P3_DIR_PU;
+
+// For the LED on Pin 7 (P3.1), I specifically want bit 1 of port 3, so I can make a bit definition like this
+
+__sbit __at (0xB0+1) LEDbit;
