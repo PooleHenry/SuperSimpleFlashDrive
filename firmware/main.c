97378b5741b202cbf1ea57dd9513c5363432265c
PooleHenry <poolehenrym@gmail.com>
2026-09-05 14:53:29 -0700

    Update main.c

--- firmware/main.c
+++ firmware/main.c
@@ -7,8 +7,23 @@ int main(void) {
 }
 
 int blinkLED(void) {
-  // led on
-  // delay somehow
-  // led off
-  // delay somehow
+  LEDbit = 1;
+  delay(50);
+  LEDbit = 0;
+  delay(50);
+}
+
+// Delay by doing some NOP instructions. 
+int delay(length) {
+  int i = 0;
+  for (i < length; i=i+1;) {
+    __asm
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    __endasm;
+  }
 }
