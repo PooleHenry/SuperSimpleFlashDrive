e40cef40e98f607fbcb787012f906b8f8d870ec5
PooleHenry <poolehenrym@gmail.com>
2026-09-05 14:07:42 -0700

    Create main.c

---
+++ firmware/main.c
@@ -0,0 +1,14 @@
+// Blink the LED
+
+#include <registers.h>
+
+int main(void) {
+  while (1) blinkLED;
+}
+
+int blinkLED(void) {
+  // led on
+  // delay somehow
+  // led off
+  // delay somehow
+}
